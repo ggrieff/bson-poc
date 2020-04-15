a3ac9edd32ddbcdd224cd54bedb50739fda715e8
ggrieff <ggrieff+githum@amazon.com>
2020-04-15 18:06:37 -0400

    properly close files

--- bson-poc.py
+++ bson-poc.py
@@ -3,10 +3,10 @@ import json
 
 i = open ('./zips.bson','rb')
 data = bson.decode_all(i.read())
-i.close
+i.close()
 
 o = open ('./zips.json','w')
 o.write(json.dumps(data))
-o.close
+o.close()
 
 
